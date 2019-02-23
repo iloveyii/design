5fb898b2db2add5867fff6ffe0a6b8f70c0e300f
Hazrat Ali <iloveyii@yahoo.com>
2019-02-23 22:42:54 +0100

    Added focus-box

--- src/App.js
+++ src/App.js
@@ -6,20 +6,46 @@ class App extends Component {
     render() {
         return (
             <div className="wrapper">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+                <header>
+                    <nav id="main-nav">
+                        <div className="wrapper">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Login</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <div className="lead-banner">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <div className="banner-content">
+                            <div className="wrapper">
+                                <div className="title">Check our products</div>
+                                <span className="sub-title">All about portfolio</span>
+                            </div>
+                        </div>
+                    </div>
                 </header>
+
+                <section id="services">
+                    <div className="wrapper">
+                        <h1>Services</h1>
+                        <p></p>
+
+                        <ul className="focus-box">
+                            <li className="service-icon"></li>
+                            <li className="service-icon"></li>
+                            <li className="service-icon"></li>
+                            <li className="service-icon"></li>
+                            <li className="service-icon"></li>
+
+                        </ul>
+                    </div>
+                </section>
+
+
             </div>
         );
     }
