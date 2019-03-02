db68a40cc6048f2933ce1ab1d7ef926bf8fab4ba
Hazrat Ali <iloveyii@yahoo.com>
2019-03-02 11:33:57 +0100

    Added focus icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -33,6 +33,56 @@ class Services extends React.Component {
 
                     </ul>
 
+                    <ul className="focus-box">
+                        <li>
+                            <div className="service-icon">
+                                <i style={Styles.parraImg}
+                                   className="pixeden glyphicon"></i>
+                                <p id="cat">مارشل</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="service-icon">
+                                <i style={Styles.roundImg}
+                                   className="pixeden glyphicon"></i>
+                                <p id="cat"> پیپلز </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="service-icon">
+                                <i style={Styles.parraImg}
+                                   className="pixeden glyphicon"></i>
+                                <p id="cat"> ملک</p>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                    <ul className="focus-box">
+                        <li>
+                            <div className="service-icon">
+                                <i style={Styles.parraImg}
+                                   className="pixeden glyphicon"></i>
+                                <p id="cat">مارشل</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="service-icon">
+                                <i style={Styles.roundImg}
+                                   className="pixeden glyphicon"></i>
+                                <p id="cat"> پیپلز </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="service-icon">
+                                <i style={Styles.parraImg}
+                                   className="pixeden glyphicon"></i>
+                                <p id="cat"> ملک</p>
+                            </div>
+                        </li>
+
+                    </ul>
+
                 </div>
             </section>
         )
