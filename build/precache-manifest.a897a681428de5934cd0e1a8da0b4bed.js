9b03be5065d9dab915431c7254f9cd1347cc88cc
Hazrat Ali <iloveyii@yahoo.com>
2019-03-02 23:20:20 +0100

    Added build

---
+++ build/precache-manifest.a897a681428de5934cd0e1a8da0b4bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aed88df23baaff066d3a1c6ecc1be3b2",
+    "url": "/static/media/banner.aed88df2.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "798c5ea45c7982abf424",
+    "url": "/static/js/main.798c5ea4.chunk.js"
+  },
+  {
+    "revision": "738bdb89effd43fff932",
+    "url": "/static/js/2.738bdb89.chunk.js"
+  },
+  {
+    "revision": "798c5ea45c7982abf424",
+    "url": "/static/css/main.f09b63f8.chunk.css"
+  },
+  {
+    "revision": "cead93eee6f44fe5937b24cc95fcf412",
+    "url": "/index.html"
+  }
+];
